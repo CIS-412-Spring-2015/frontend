a840033514bce20c8b3c3ebef555dac36c623268
Bekah <neongreenfruit@gmail.com>
2015-02-10 11:03:40 -0500

    Little tweaks to Ministry Module Stuff

--- app/ministry/route.js
+++ app/ministry/route.js
@@ -1,5 +1,4 @@
 import AbstractModuleRoute from 'hospitalrun/routes/abstract-module-route';
-//import PatientId from 'hospitalrun/mixins/patient-id';
 export default AbstractModuleRoute.extend({
     addCapability: '',
     additionalModels: [{ 
@@ -8,27 +7,26 @@ export default AbstractModuleRoute.extend({
     moduleName: 'ministry',
     newButtonText: '+ new ministry event',
     sectionTitle: 'Ministry',
-    subActions: []
-//    {
-//        text: 'Demographics',
-//        linkTo: 'ministry.demographics'
-//    }, {
-//        text: 'Hospital',
-//        linkTo: 'ministry.hospital'
-//    }, {
-//        text: 'Leadership',
-//        linkTo: 'ministry.leadership'
-//    }, {
-//        text: 'Community',
-//        linkTo: 'ministry.community'
-//    }, {
-//        text: 'Christianity Explored',
-//        linkTo: 'ministry.christianityExplored'
-//    }, {
-//        text: 'Faith Declarations',
-//        linkTo: 'ministry.faithDeclarations'
-//    }, {
-//        text: 'Additional Information',
-//        linkTo: 'ministry.additionalInformation'
-//    }]
+    subActions: [{
+        text: 'Demographics',
+        linkTo: 'ministry.demographics'
+    }, {
+        text: 'Hospital',
+        linkTo: 'ministry.hospital'
+    }, {
+        text: 'Leadership',
+        linkTo: 'ministry.leadership'
+    }, {
+        text: 'Community',
+        linkTo: 'ministry.community'
+    }, {
+        text: 'Christianity Explored',
+        linkTo: 'ministry.christianityExplored'
+    }, {
+        text: 'Faith Declarations',
+        linkTo: 'ministry.faithDeclarations'
+    }, {
+        text: 'Additional Information',
+        linkTo: 'ministry.additionalInformation'
+    }]
 });
--- app/router.js
+++ app/router.js
@@ -63,13 +63,13 @@ Router.map(function() {
     this.resource('ministry', function() {
         this.route('edit', { path: "/edit/:ministry_id" });
         this.route('report');
-//        this.route('demographics');
-//        this.route('hospital');
-//        this.route('leadership');
-//        this.route('community');
-//        this.route('christianityExplored');
-//        this.route('faithDeclarations');
-//        this.route('additionalInformation');
+        this.route('demographics');
+        this.route('hospital');
+        this.route('leadership');
+        this.route('community');
+        this.route('christianityExplored');
+        this.route('faithDeclarations');
+        this.route('additionalInformation');
     });
     
     this.resource('patients', function() {
