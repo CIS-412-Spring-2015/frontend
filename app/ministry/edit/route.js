fdb6eca9fae5dd6f4f0c12ec7b647f8c44e89b49
Bekah <neongreenfruit@gmail.com>
2015-02-12 10:08:13 -0500

    Adding basics for our module pieces.
    new report, in progress and report archive

---
+++ app/ministry/edit/route.js
@@ -0,0 +1,5 @@
+import AbstractIndexRoute from 'hospitalrun/routes/abstract-index-route';
+export default AbstractIndexRoute.extend({
+    modelName: 'ministry',
+    pageTitle: 'Edit Monthly Report - (Enter Month and Year Here)'
+    });
