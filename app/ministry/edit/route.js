368a4e3457be1b219aaea1eef866b2ba7988f3e4
Peter Jones <pj11607@gmail.com>
2015-02-13 10:54:08 -0500

    Updated edit route for proper title (Edit Report)

--- app/ministry/edit/route.js
+++ app/ministry/edit/route.js
@@ -1,5 +1,6 @@
-import AbstractIndexRoute from 'hospitalrun/routes/abstract-index-route';
-export default AbstractIndexRoute.extend({
+import AbstractEditRoute from 'hospitalrun/routes/abstract-edit-route';
+export default AbstractEditRoute.extend({
     modelName: 'ministry',
-    pageTitle: 'Edit Monthly Report - {{currentMonth}}'
+    editTitle: 'Edit Monthly Report - {{currentMonth}}',
+    newTitle: 'New Report - {{currentMonth}}'
     });
