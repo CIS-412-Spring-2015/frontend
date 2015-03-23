7dce3cc43e8e077b6fa328c837a6714b7458ffea
Bekah <neongreenfruit@gmail.com>
2015-03-23 12:46:13 -0400

    creating in progress reports page

--- app/ministry/report/controller.js
+++ app/ministry/report/controller.js
@@ -2,6 +2,7 @@ import AbstractPagedController from 'hospitalrun/controllers/abstract-paged-cont
 import PouchAdapterUtils from "hospitalrun/mixins/pouch-adapter-utils";
 import UserSession from "hospitalrun/mixins/user-session";
 export default AbstractPagedController.extend(PouchAdapterUtils, UserSession,{
+    addPermission: 'add_ministry_report',
     
     canAddReport: function() {        
         return this.currentUserCan('add_ministry_report');
