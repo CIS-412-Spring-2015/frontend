80331cbaf88603059c57d77ad3888011558304b0
Peter Jones <Pj11607@gmail.com>
2015-04-10 11:13:18 -0400

    Redistribution
    reformatted hospital tab, separated files, saved data to database

--- app/models/ministry.js
+++ app/models/ministry.js
@@ -1,12 +1,21 @@
 import AbstractModel from "hospitalrun/models/abstract";
 import ReportDate from 'hospitalrun/mixins/dob-days';
-import Ember from "ember";
 
 export default AbstractModel.extend(ReportDate, {
     // Report Metadata //
     reportDate: DS.attr('date'),
     reportArchived: DS.attr('boolean'),
 
+    //Hospital Tab Data
+        //Staff View
+        entireStaffDevotions: DS.attr('number'),
+        departmentDevotions: DS.attr('number'),
+        hospitalPrayerGroupIntercessions: DS.attr('number'),
+        spiritualCenterStaffMeetings: DS.attr('number'),
+        staffDevelopment: DS.attr('string'),
+        staffOpportunities: DS.attr('string'),
+        //Patient View
+    
     // New Believers Section
     // believers: DS.hasMany('new-believers'),
     believers: DS.attr(),
