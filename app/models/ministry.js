fb3eb527cb129f3633b7693beca46da8fc7189c9
zmarkle <zm1163@messiah.edu>
2015-04-21 13:12:30 -0500

    Add Leadership and Add Participant working

--- app/models/ministry.js
+++ app/models/ministry.js
@@ -23,10 +23,9 @@ export default AbstractModel.extend(ReportDate, {
     believers: DS.attr(),
     
     // Events Section
-    leadEvents: DS.hasMany('leadership-event'),//what will need to work
-    //leadEvents: DS.attr(), // Currently working with
-    //commEvents: DS.attr(),
-    commEvents: DS.hasMany('community-event'),//what will need to work
+    leadEvents: DS.hasMany('leadership-event'),
+    commEvents: DS.hasMany('community-event'),
+    participants: DS.hasMany('leadership-participant'),
 
     //   Additional Info View    //
     ministryResults: DS.attr('string'),
